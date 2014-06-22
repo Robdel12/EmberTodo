8554dd2bd7ac202ab33f8a380446f0589bb21829
Robert DeLuca <robertd@izea.com>
2014-06-22 12:19:01 -0400

    Use local storage for tests

--- js/application.js
+++ js/application.js
@@ -1,11 +1,12 @@
 window.Todos = Ember.Application.create();
-
-// How to get this to only run for tests?
-// Todos.ApplicationAdapter = DS.LSAdapter.extend({
-//   namespace: 'todos-emberjs'
-// });
-
-Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
-  firebase: new Firebase("https://boiling-fire-1133.firebaseio.com")
-});
-Todos.ApplicationSerializer = DS.FirebaseSerializer.extend();
+if (Ember.testing){
+  // How to get this to only run for tests?
+  Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+  });
+} else {
+  Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
+    firebase: new Firebase("https://boiling-fire-1133.firebaseio.com")
+  });
+  Todos.ApplicationSerializer = DS.FirebaseSerializer.extend();
+}
